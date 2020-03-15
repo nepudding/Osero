d72895901b614337dc25b3873a07110a3963f0c1
hash <17024120@mmm.muroran-it.ac.jp>
2020-03-15 19:58:48 +0900

    ゲーミングオセロをつくるよ

--- page/Othello.js
+++ page/Othello.js
@@ -1,36 +1,69 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-const STONE_SIZE = 35;
+const STONE_SIZE = 40;
 const STAGE_SIZE = 8;
+var COLOR = [128,120,120];
 
+function getBlack(){
+    var col = "#";
+    for(var i=0;i<3;i++){
+        col += ("0"+COLOR[i].toString(16)).slice(-2);
+    }
+    return col;
+}
+function getWhite(){
+    var col = "#";
+    for(var i=0;i<3;i++){
+        col += ("0"+(255-COLOR[i]).toString(16)).slice(-2);
+    }
+    return col;
+}
 class Stone{
-    constructor(x,y){
+    constructor(x,y,side){
         this.x = x;
         this.y = y;
+        this.side = side;
     }
     draw(){
+        if(this.side == 0) return;
         ctx.beginPath();
         ctx.arc(this.x,this.y,STONE_SIZE,0,Math.PI*2);
-        ctx.fillStyle = "#555555";
+        if(this.side == 1)ctx.fillStyle = getBlack();
+        if(this.side == 2)ctx.fillStyle = getWhite();
         ctx.fill();
         ctx.closePath();
     }
 }
 
-// ここからmainだよ
-var stones = []
-for(var i=0; i<STAGE_SIZE;i++){
-    stones[i] = [];
-    for(var j=0;j<STAGE_SIZE;j++){
-        stones[i][j] = new Stone(100*i+50,100*j+50);
+class Board{
+    constructor(){
+        this.stones = []
+        for(var i=0; i<STAGE_SIZE;i++){
+            this.stones[i] = [];
+            for(var j=0;j<STAGE_SIZE;j++){
+                this.stones[i][j] = new Stone(100*i+50,100*j+50,(i+j)%3);
+            }
+        }
+    }
+    draw(){
+        for(var i=0; i<STAGE_SIZE;i++){
+            for(var j=0;j<STAGE_SIZE;j++){
+                this.stones[i][j].draw();
+            }
+        }
     }
 }
-document.write("oiu")
+
+var board = new Board();
 
 
-for(var i=0; i<STAGE_SIZE;i++){
-    for(var j=0;j<STAGE_SIZE;j++){
-        stones[i][j].draw();
+board.draw()
+
+function draw(){
+    board.draw()
+    for(var i=0;i<3;i++){
+        COLOR[i] = (COLOR[i]+i+3)%256
     }
 }
+setInterval(draw,10);
